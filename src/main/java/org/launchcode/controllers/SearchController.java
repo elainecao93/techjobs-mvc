5e4059b2eaea44ba07b2014a685cf35dc791c71d
Elaine Cao <47513241+elainecao93@users.noreply.github.com>
2019-06-05 14:30:21 -0500

    finished studio

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,5 +24,32 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results")
+    public String searchResults(Model model, HttpServletRequest request) {
+
+        String searchType = request.getParameter("searchType");
+        String searchTerm = request.getParameter("searchTerm");
+
+        ArrayList<HashMap<String, String>> output;
+
+        if (searchTerm == null || searchTerm.length() == 0) {
+            output = JobData.findAll();
+        }
+        else if (searchType.equals("all")) {
+            output = JobData.findByValue(searchTerm);
+        }
+        else {
+            output = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        assert(output != null);
+
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("output", output);
+
+        return "results"; //TODO
+    }
 
 }
